25bf8f9b623cf1d3b4cebf48956ba1960638caa4
SonnieCodes <mgathoni.gathoni9@gmail.com>
2024-11-20 05:28:44 +0300

    loop

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+function writeCards(name, event){
+
+  const messages = [];
+
+  for (let i = 0; i<name.length; i++){
+
+    const message = `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+
+    messages.push(message);
+    
+  }
+  return messages;
+
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number); 
+    number--; 
+  }
+}
